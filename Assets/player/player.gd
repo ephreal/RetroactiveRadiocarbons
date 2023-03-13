76fd5639a7f744ad3802ba6d9abddde8a54a24f9
ephreal <tjannx@hotmail.com>
2023-03-12 20:27:09 -0700

    Adds slight jump cheating in player's favor

--- Assets/player/player.gd
+++ Assets/player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 var WALK_SPEED = 250
 var JUMP_SPEED = 300
+var JUMP_FRAMES = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,8 +18,15 @@ func _physics_process(delta):
 		velocity.x =  WALK_SPEED
 	else:
 		velocity.x = 0
-		
-	if Input.is_action_just_pressed("jump") and self.is_on_floor():
+	
+	
+	self.check_jumping()
+	# "move_and_slide" already takes delta time into account.
+	self.move_and_slide()
+
+
+func check_jumping():
+	if self.can_jump() and Input.is_action_just_pressed("jump"):
 		velocity.y -= self.JUMP_SPEED
 	elif (Input.is_action_just_pressed("jump") or Input.is_action_just_pressed("ui_down")) and self.is_on_ceiling():
 		velocity.y += self.JUMP_SPEED / 2
@@ -27,5 +35,14 @@ func _physics_process(delta):
 	elif self.is_on_ceiling():
 		velocity.y = 1
 
-	# "move_and_slide" already takes delta time into account.
-	self.move_and_slide()
+
+func can_jump():
+	if self.is_on_floor():
+		self.JUMP_FRAMES = 5
+		return true
+	
+	self.JUMP_FRAMES -= 1
+	
+	if self.JUMP_FRAMES > 0:
+		return true
+	
