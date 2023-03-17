451a35f73263b97a83379fa46f73943da24819bf
ephreal <tjannx@hotmail.com>
2023-03-16 21:55:30 -0700

    Adds animation to Genessa

--- Assets/characters/genessa/Genessa.gd
+++ Assets/characters/genessa/Genessa.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 var camera
+var sprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.camera = self.get_node("camera")
+	self.sprite = self.get_node("AnimatedSprite2D")
 
 
 func _physics_process(delta):
@@ -16,6 +18,15 @@ func _physics_process(delta):
 		velocity.x =  GameVars.WALK_SPEED
 	else:
 		velocity.x = 0
+	
+	if Input.is_action_pressed("ui_left"):
+		self.sprite.play("walk")
+		self.sprite.flip_h = false
+	elif Input.is_action_pressed("ui_right"):
+		self.sprite.play("walk")
+		self.sprite.flip_h = true
+	else:
+		self.sprite.stop()
 	
 	
 	Player.check_jumping()
